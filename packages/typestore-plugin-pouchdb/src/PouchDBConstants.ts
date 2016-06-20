9845ab50fa5db022f5389c19b5c949bb2cf429ba
Jonathan Glanz <jonathan.glanz@gmail.com>
2016-06-19 21:32:31 -0400

    - formatted operators

--- packages/typestore-plugin-pouchdb/src/PouchDBConstants.ts
+++ packages/typestore-plugin-pouchdb/src/PouchDBConstants.ts
@@ -3,12 +3,29 @@ export const PouchDBFnFinderKey = 'pouchdb:fn-finder'
 export const PouchDBFilterFinderKey = 'pouchdb:filter-finder'
 
 export const PouchDBPKIndex = '__idx_ts_pk'
+
 export const PouchDBTypeIndex = '__idx_ts_type'
+
 export const PouchDBAttributePrefix = 'attrs.'
+
 export const PouchDBReservedFields = ['type']
+
 export const PouchDBOperators = [
-	'$lt', '$gt', '$lte', '$gte', '$eq',
-	'$exists', '$type', '$in', '$nin', '$all',
-	'$size', '$or', '$nor', '$not', '$mod',
-	'$regex', '$elemMatch'
+	'$lt',
+	'$gt',
+	'$lte',
+	'$gte',
+	'$eq',
+	'$exists',
+	'$type',
+	'$in',
+	'$nin',
+	'$all',
+	'$size',
+	'$or',
+	'$nor',
+	'$not',
+	'$mod',
+	'$regex',
+	'$elemMatch'
 ]
